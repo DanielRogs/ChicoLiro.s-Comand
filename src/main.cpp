33f32c5a8010358c0c77d7972bfb40e210815f74
Angelicahaas <harleny87@gmail.com>
2024-05-29 15:16:30 -0300

    Fix bugs

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,35 @@
 #include <Arduino.h>
-#include "motor/motor.h"  
+#define pinMot1A 5
+#define pinMot1B 6
+#define pinMot2A 9
+#define pinMot2B 10
+
+// put function declarations here:
 
 void setup() {
-    Serial.begin(9600);
-    setupMotor();
+  // put your setup code here, to run once:
+  pinMode(pinMot1A, OUTPUT);
+  pinMode(pinMot1B, OUTPUT);
+  pinMode(pinMot2A, OUTPUT);
+  pinMode(pinMot2B, OUTPUT);
+
 }
 
 void loop() {
-    loopMotor();
+  // put your main code here, to run repeatedly:
+  digitalWrite(pinMot1A, HIGH);
+  delay(2000);
+  digitalWrite(pinMot1A, LOW);
+  digitalWrite(pinMot1B, HIGH);
+  delay(2000);
+  digitalWrite(pinMot1B, LOW);
 
-    // Desliga o motor após 50 segundos para o teste
-    if (millis() > 50000) {
-        desligarMotor();
-    }
-
+  digitalWrite(pinMot2A, HIGH);
+  delay(2000);
+  digitalWrite(pinMot2A, LOW);
+  digitalWrite(pinMot2B, HIGH);
+  delay(2000);
+  digitalWrite(pinMot2B, LOW);
 }
+
+// put function definitions here:
