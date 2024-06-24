b0505fa28595a5d4426fdbb77d19528193c0f094
DanielRogs <danielrocha.dr142@gmail.com>
2024-06-24 14:11:17 -0300

    feat: incluindo todos os sensores e wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,299 @@
-#include <Arduino.h>
-#define pinMot1A 5
-#define pinMot1B 6
-#define pinMot2A 9
-#define pinMot2B 10
+/*
+  Título: SISTEMA CONTROLADOR DO CARRINHO
+  Autores: Daniel Rodrigues da Rocha
+  Descrição: Código para controlar um carrinho seguidor de linha utilizando sensores infravermelhos e controle PWM.
+  Data de criação: 19/06/2024
+*/
 
-// put function declarations here:
+/* MAPEAMENTO DE HARDWARE */
+// Conexões do motor (Os pinos de Enable EN_A e EN_B devem ser pinos de PWM para podermos controlar a velocidade dos motores)
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <HTTPClient.h>
+
+// Definindo as credenciais WiFi
+const char* ssid = "UNB Wireless";
+const char* wifi_password = "SenhaAqui";
+
+// Definindo o URL do servidor
+const char* serverUrl = "https://chicoliro.xobengala.com.br/api/dados/receive-data";
+const char* serverFailUrl = "https://chicoliro.xobengala.com.br/api/trilha/failed";
+
+// Motor A - CORRIGIR
+#define IN1 12
+#define IN2 14
+#define EN_A 13
+
+// Motor B
+#define IN3 27
+#define IN4 26
+#define EN_B 25
+
+// Conexões do infravermelho
+#define pinSensorMeio 2
+#define pinSensorEsq 15
+#define pinSensorDir 4
+
+// Conexões dos encoders
+const byte MOTOR_A = 15;
+const byte MOTOR_B = 3;
+volatile int counter_A, counter_B = 0;
+unsigned long previousMillis = 0;
+const unsigned long interval = 1000; // Intervalo de 1 segundo
+const float diskslots = 20.00;
+const float wheeldiameter = 6.50;
+
+// Variáveis dos encoders:
+float rotation_A, rotation_B = 0.0;
+
+// Variáveis de controle de velocidade
+const int velocidadeBase = 50;
+const int velocidadeMaxima = 80;
+const int ajusteVelocidade = 15;
+const int limitePreto = 80;
+
+// Variáveis de leitura de tensão
+double vadc = 3.3; // Tensão de referência do conversor analógico-digital (VADC)
+double voltage_in; // Variável para armazenar a tensão lida
+double real_voltage; // Variável para armazenar a tensão real
+double R1 = 81.6; // Valor da resistência R1
+double R2 = 26.5; // Valor da resistência R2
+
+// Variáveis para a requisição
+volatile bool isMoving = false;
+
+// Variáveis para aperfeiçoar o controle:
+int naoDetectadoCounter = 0; // Contador para condição de não detectado
+const int naoDetectadoLimite = 10; // Limite para parar os motores
+
+AsyncWebServer server(80);
+
+void calculateSpeed() {
+
+  // CALCULO DO RPM DO MOTOR A
+  rotation_A = ((counter_A / diskslots) * 60.00) / 2;
+  // float speed_A = (rotation_A * wheeldiameter * 3.14) / 60;
+
+  // CALCULO DO RPM DO MOTOR B
+  rotation_B = ((counter_B / diskslots) * 60.00) / 2;
+  // float speed_B = (rotation_B * wheeldiameter * 3.14) / 60;
+
+  counter_A = 0;
+  counter_B = 0;
+}
+
+// Função de interrupção para contar pulsos do motor A
+void ISR_count1() {
+  counter_A++;
+}
+
+// Função de interrupção para contar pulsos do motor B
+void ISR_count2() {
+  counter_B++;
+}
+
+void lerSensoresTensao() {
+  voltage_in = ((double)analogRead(2) / 1024) * vadc * 0.2909;
+  real_voltage = voltage_in * ((R1 + R2) / R2) * 2.97330697;
+}
+
+void enviarDados() {
+  HTTPClient http;
+  http.begin(serverUrl);
+
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  String jsonData = "{\"rpmMotorDir\": " + String(rotation_A) +
+                      ", \"rpmMotorEsq\": " + String(rotation_B) +
+                      ", \"tensao\": " + String(9) +
+                      ", \"isMoving\": " + String(isMoving) + "}"; 
+  
+  int httpResponseCode = http.POST(jsonData);
+  http.end();
+}
+
+void enviarFalha() {
+  HTTPClient http;
+  http.begin(serverFailUrl);
+
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  String jsonData = "{}"; 
+  
+  int httpResponseCode = http.POST(jsonData);
+  http.end();
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(pinMot1A, OUTPUT);
-  pinMode(pinMot1B, OUTPUT);
-  pinMode(pinMot2A, OUTPUT);
-  pinMode(pinMot2B, OUTPUT);
+  // Configuração dos pinos dos motores
+  pinMode(EN_A, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(EN_B, OUTPUT);
 
+  // Configuração dos pinos dos sensores infravermelhos
+  pinMode(pinSensorMeio, INPUT);
+  pinMode(pinSensorEsq, INPUT);
+  pinMode(pinSensorDir, INPUT);
+
+  // Configuração do sensor de tensão
+  pinMode(2, INPUT);
+
+  // Inicializa todos os pinos dos motores em nível lógico baixo
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+
+  analogWrite(EN_A, 0);
+  analogWrite(EN_B, 0);
+
+  Serial.begin(9600);
+
+  // Configuração do encoder
+  attachInterrupt(digitalPinToInterrupt(MOTOR_A), ISR_count1, RISING);
+  attachInterrupt(digitalPinToInterrupt(MOTOR_B), ISR_count2, RISING);
+
+  // Conecte-se ao WiFi
+  WiFi.begin(ssid, wifi_password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    // Serial.println("Conectando ao WiFi...");
+  }
+  // Serial.println("Conectado ao WiFi");
+
+  // Inicie o servidor
+  server.begin();
+
+  xTaskCreate([](void*) {
+    for (;;) {
+      lerSensoresTensao();
+      if (isMoving) {
+        enviarDados();
+      }
+      vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+  }, "EnviarDadosTask", 4096, NULL, 1, NULL);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(pinMot1A, HIGH);
-  delay(2000);
-  digitalWrite(pinMot1A, LOW);
-  digitalWrite(pinMot1B, HIGH);
-  delay(2000);
-  digitalWrite(pinMot1B, LOW);
+  // Leitura dos sensores infravermelhos
+  int sensorMeio = analogRead(pinSensorMeio);
+  int sensorEsq = analogRead(pinSensorEsq);
+  int sensorDir = analogRead(pinSensorDir);
 
-  digitalWrite(pinMot2A, HIGH);
-  delay(2000);
-  digitalWrite(pinMot2A, LOW);
-  digitalWrite(pinMot2B, HIGH);
-  delay(2000);
-  digitalWrite(pinMot2B, LOW);
+  // LEITURA DOS ENCODERS
+  if (millis() - previousMillis >= interval) {
+    calculateSpeed();
+    previousMillis = millis();
+  }
+
+  if (sensorMeio > limitePreto && sensorEsq <= limitePreto && sensorDir <= limitePreto) {
+    // Linha reta - ambos os motores com velocidade base
+    // moverFrente(velocidadeBase, velocidadeBase);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    analogWrite(EN_A, velocidadeBase);
+
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(EN_B, velocidadeBase);
+    isMoving = true;
+    naoDetectadoCounter = 0; // Resetar contador
+  }
+  else if (sensorEsq > limitePreto && sensorDir <= limitePreto) {
+    // Virar à esquerda - reduzir velocidade do motor esquerdo
+    // moverFrente(0, velocidadeMaxima);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(EN_A, velocidadeMaxima - ajusteVelocidade);
+
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(EN_B, velocidadeMaxima);
+    isMoving = true;
+    naoDetectadoCounter = 0; // Resetar contador
+  }
+  else if (sensorDir > limitePreto && sensorEsq <= limitePreto) {
+    // Virar à direita - reduzir velocidade do motor direito
+    // moverFrente(velocidadeMaxima, 0);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    analogWrite(EN_A, velocidadeMaxima);
+
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(EN_B, velocidadeMaxima - ajusteVelocidade);
+    isMoving = true;
+    naoDetectadoCounter = 0; // Resetar contador
+  }
+  else if (sensorDir > limitePreto && sensorEsq <= limitePreto && sensorMeio > limitePreto) {
+    // Virar à direita - reduzir velocidade do motor direito
+    // moverFrente(velocidadeBase, 0);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    analogWrite(EN_A, velocidadeBase);
+
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(EN_B, velocidadeBase - ajusteVelocidade);
+    isMoving = true;
+    naoDetectadoCounter = 0; // Resetar contador
+  }
+  else if (sensorDir <= limitePreto && sensorEsq > limitePreto && sensorMeio > limitePreto) {
+    // Virar à direita - reduzir velocidade do motor esquerdo
+    // moverFrente(0, velocidadeBase);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(EN_A, velocidadeBase - ajusteVelocidade);
+
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(EN_B, velocidadeBase);
+    isMoving = true;
+    naoDetectadoCounter = 0; // Resetar contador
+  }
+  else if (sensorMeio > limitePreto && sensorEsq > limitePreto && sensorDir > limitePreto) {
+    // Fim do percurso - parar o carrinho
+    pararMotores();
+    isMoving = false;
+    naoDetectadoCounter = 0; // Resetar contador
+    enviarDados(); // Enviar dados de parada
+  }
+  else {
+    // Não detectado - Continuar andando com velocidade reduzida
+    moverFrente(velocidadeBase - ajusteVelocidade, velocidadeBase - ajusteVelocidade);
+    isMoving = true;
+    naoDetectadoCounter++;
+    if (naoDetectadoCounter >= naoDetectadoLimite) {
+      pararMotores(); // Parar os motores se o limite for alcançado
+      isMoving = false;
+      enviarFalha();
+    }
+  }
+
 }
 
-// put function definitions here:
+// Função para mover o carrinho para frente com controle de velocidade
+void moverFrente(int velocidadeA, int velocidadeB) {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(EN_A, velocidadeA);
+
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(EN_B, velocidadeB);
+}
+
+// Função para parar os motores
+void pararMotores() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  analogWrite(EN_A, 0);
+
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(EN_B, 0);
+}
